b7d5700adc1951a38b37c3bf9072b28ae715d2f6
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2026-05-07 00:10:26 -0300

    feat: adiciona consultas personalizadas no AlunoRepository

--- src/main/java/com/centrotreinamento/bjj/repository/AlunoRepository.java
+++ src/main/java/com/centrotreinamento/bjj/repository/AlunoRepository.java
@@ -1,11 +1,19 @@
 package com.centrotreinamento.bjj.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.centrotreinamento.bjj.domain.Aluno;
+import com.centrotreinamento.bjj.domain.enums.Faixa;
 
 public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
     
+    Optional<Aluno> findByEmail(String email);
+
+    List<Aluno> findByFaixa(Faixa faixa);
+
+    List<Aluno> findByAtivoTrue();
 }
