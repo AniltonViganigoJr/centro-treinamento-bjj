464b36391e9e37e1ce3093362af91a7fc8936edc
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2026-05-19 01:10:09 -0300

    feat: adiciona AlunoMapper

---
+++ src/main/java/com/centrotreinamento/bjj/mapper/AlunoMapper.java
@@ -0,0 +1,34 @@
+package com.centrotreinamento.bjj.mapper;
+
+import com.centrotreinamento.bjj.domain.Aluno;
+import com.centrotreinamento.bjj.dto.request.AlunoRequestDTO;
+import com.centrotreinamento.bjj.dto.response.AlunoResponseDTO;
+
+public class AlunoMapper {
+    
+    private AlunoMapper(){}
+
+    public static Aluno toEntity(AlunoRequestDTO dto) {
+
+        return new Aluno(
+            dto.id(),
+            dto.nome(),
+            dto.idade(),
+            dto.email(),
+            dto.telefone()
+        );
+    }
+
+    public static AlunoResponseDTO toResponseDTO(Aluno aluno) {
+
+        return new AlunoResponseDTO(
+                aluno.getId(),
+                aluno.getNome(),
+                aluno.getEmail(),
+                aluno.getTelefone(),
+                aluno.getDataMatricula(),
+                aluno.isAtivo(),
+                aluno.getFaixa(),
+                aluno.getGraus());
+    }
+}
