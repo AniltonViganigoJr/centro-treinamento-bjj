f06f722a329fe86128cd8818bc47696dc9938c36
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2026-06-08 15:23:57 -0300

    feat: adiciona validações no AlunoRequest com Bean Validation

--- src/main/java/com/centrotreinamento/bjj/dto/request/AlunoRequestDTO.java
+++ src/main/java/com/centrotreinamento/bjj/dto/request/AlunoRequestDTO.java
@@ -1,15 +1,17 @@
 package com.centrotreinamento.bjj.dto.request;
 
-import java.util.UUID;
 
-import com.centrotreinamento.bjj.domain.enums.Faixa;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record AlunoRequestDTO(
-    UUID id,
-    String nome,
-    int idade,
-    String email,
-    String telefone,
-    Faixa faixa,
-    int graus
-) {}
+    @NotBlank @Size(min = 3, max = 100) String nome,
+    @Min(4) @Max(90) int idade,
+    @NotBlank @Email String email,
+    @NotBlank @Pattern(regexp = "\\d{11}", message = "Telefone deve contar 11 dígitos") String telefone) {
+}
